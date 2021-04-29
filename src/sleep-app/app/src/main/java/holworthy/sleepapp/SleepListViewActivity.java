0ef3119c0e830959f4300c4b0e774dc053abfa9c
Jack Demonakis <jackdemonakis@gmail.com>
2021-04-29 16:36:16 +0100

    merge and starting sleep list view

---
+++ src/sleep-app/app/src/main/java/holworthy/sleepapp/SleepListViewActivity.java
@@ -0,0 +1,14 @@
+package holworthy.sleepapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SleepListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep_list_view);
+    }
+}
